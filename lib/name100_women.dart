c45c90f3e709eef5c49a00deae8d6e533f2a7d68
ilovejackylee <ilovejackylee@proton.me>
2026-09-13 10:54:59 +0800

    Initial release: name100_women 0.1.0 for pub.dev

---
+++ lib/name100_women.dart
@@ -0,0 +1,8 @@
+/// Offline practice companion for the Name 100 Women challenge.
+library;
+
+/// Play URL for the online Wikidata-checked Name 100 Women challenge.
+const String playUrl = 'https://nameahundred.com/women/';
+
+/// Package version.
+const String packageVersion = '0.1.0';
